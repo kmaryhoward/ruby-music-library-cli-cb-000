f734ccb425faaea62502f3a4c16f644b690c1038
Kathleen Howard <kmaryhoward@gmail.com>
2019-07-06 19:26:18 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -69,12 +69,12 @@ class MusicLibraryController
       puts "Which song number would you like to play?"
         input = gets.strip.to_i
           if input < Song.all.length
-              
-              list_songs.select.with_index do |song, index| 
+
+              list_songs.select.with_index do |song, index|
                 if index == input
                   puts "Playing #{song.name} by #{song.artist.name}"
-          
-          
-          
+
+
+
   end
 end
