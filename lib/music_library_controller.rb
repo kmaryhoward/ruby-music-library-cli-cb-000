e6e677a95e4e32493c2f98e09749d84d57a261c7
Kathleen Howard <kmaryhoward@gmail.com>
2019-07-06 18:24:58 +0000

    Automatically backed up by Learn

---
+++ lib/music_library_controller.rb
@@ -0,0 +1,48 @@
+class MusicLibraryController
+
+  attr_accessor :path, :importer
+
+  def initialize(path = './db/mp3s')
+    @path = path
+    @importer = MusicImporter.new(path)
+    importer.import
+
+  end
+
+  def call
+    puts "Welcome to your music library!"
+    puts "To list all of your songs, enter 'list songs'."
+    puts "To list all of the artists in your library, enter 'list artists'."
+    puts "To list all of the genres in your library, enter 'list genres'."
+    puts "To list all of the songs by a particular artist, enter 'list artist'."
+    puts "To list all of the songs of a particular genre, enter 'list genre'."
+    puts "To play a song, enter 'play song'."
+    puts "To quit, type 'exit'."
+    puts "What would you like to do?"
+
+    input = gets.strip
+    if input == "exit"
+      return "exit"
+    else
+      call
+    end
+
+  end
+
+  def list_songs
+    songs = Song.all.sort_by(&:name)
+    songs.each_with_index {|song, index| puts "#{index+1}. #{song.artist.name} - #{song.name} - #{song.genre.name}" }
+
+
+  end
+
+  def list_artists
+    artists = Artist.all.sort_by(&:name)
+    artists.each_with_index{|artist, index| puts "#{index+1}. #{artist.name}"}
+  end
+
+  def list_genres
+    genres = Genre.all.sort_by(&:name)
+    genres.each_with_index{|genre, index| puts "#{index+1}. #{genre.name}"}
+  end
+end
