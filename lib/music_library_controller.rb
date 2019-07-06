32ffceeaaead52dbd8b79e2413ac9f57e7e5310d
Kathleen Howard <kmaryhoward@gmail.com>
2019-07-06 19:01:48 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -51,7 +51,7 @@ class MusicLibraryController
     input = gets.strip
       artist = Artist.find_by_name(input)
       songs = artist.songs.sort_by(&:name)
-      songs.sort_by(&:name).each_with_index{|song, index| puts "#{index+1}. #{song.name} - #{song.genre}"}
+      songs.sort_by(&:name).each_with_index{|song, index| puts "#{index+1}. #{song.name} - #{song.genre.name}"}
 
 
   end
